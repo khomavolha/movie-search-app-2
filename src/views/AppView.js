80f4e2a29060765251bbb51e327c3e1c29b1810c
khomavolha <herdadevierhd@gmail.com>
2020-05-07 23:51:16 +0300

    feat: add slider

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -1,13 +1,58 @@
+import Swiper from 'swiper';
+import '../../node_modules/swiper/css/swiper.css';
+
 export default class AppView {
-  constructor(clips) {
-    this.clips = clips;
+  constructor(title) {
+    this.title = title;
+    console.log(title);
   }
 
-  render() {
+  async render() {
+    const swiperContainer = document.querySelector('.swiper-container');
     const swiperWrapper = document.querySelector('.swiper-wrapper');
-    const content = document.createElement('ul');
-    content.innerHTML = this.clips.map((title) => `<li>${title}</li>`).join('');
 
-    swiperWrapper.appendChild(content);
+    const movieTitle = this.title.movieTitle.map(
+      (title) => `<div class='movieTitle'>${title}</div>`
+    );
+
+    const moviePoster = this.title.moviePoster.map(
+      (poster) => `<div class='moviePoster'><img src='${poster}'></div>`
+    );
+
+    const movieYear = this.title.movieYear.map(
+      (year) => `<div class='movieYear'>${year}</div>`
+    );
+
+    //сложить все массивы
+    const filmsArray = movieTitle.map((value, index) => {
+      return value + moviePoster[index] + movieYear[index];
+    });
+    //console.log(filmsArray);
+
+    swiperWrapper.innerHTML = filmsArray
+      .map((content) => `<div class="swiper-slide">${content}</div>`)
+      .join('');
+
+    // eslint-disable-next-line no-unused-vars
+    const res = await swiperContainer.prepend(swiperWrapper);
+
+    // eslint-disable-next-line no-unused-vars
+    const mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slideClass: 'swiper-slide',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
   }
 }
